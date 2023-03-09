988a06e80a7ef1c8ba703efc3ba26a143e774524
victoriasmuk <vicsmuk@icloud.com>
2023-03-09 14:44:18 -0500

    first react with jsx complete

---
+++ react/first_jsx/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to learn to do:</h3>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
